1e6dbe2975653983ed2b42db289d01936b94713d
Mark Kimsal <metrofindings@gmail.com>
2015-02-27 13:29:25 -0500

    Flesh out form more.

--- templates/selfsign01/index.html.php
+++ templates/selfsign01/index.html.php
@@ -42,12 +42,7 @@
     <div class="row">
         <div class="col-sm-6">
             <form>
-                <h3>Public Private Key Gen</h3>
-                <div class=form-group">
-                    <label>Country
-                    <input type="text" class="form-control" id="keygen-country" placeholder="UK, CA, US, ...">
-                    </label>
-                </div>
+                <h3>1. Public Private Key Gen</h3>
                 <div class=form-group">
                     <label>Strength
                     <select class="form-control" id="keygen-bits">
@@ -55,6 +50,13 @@
                     <option value="2048" selected="selected">2048</option>
                     </select>
                     </label>
+				    <label>Type
+                    <select class="form-control" id="keygen-type">
+                    <option value="dsa">dsa</option>
+                    <option value="rsa" selected="selected">rsa</option>
+                    </select>
+                    </label>
+
                 </div>
 
             </form>
@@ -70,18 +72,57 @@
         <div class="col-sm-6">
             <form>
 
-                <h3>Certificate Sign Request (CSR)</h3>
+                <h3>2. Certificate Sign Request (CSR)</h3>
                 <div class=form-group">
-                    <label for="hostname">hostname:</label>
-                    <input type="hostname" class="form-control" id="hostname" placeholder="www.example.com">
+                    <label>Country
+                    <input type="text" class="form-control" id="csr-country" placeholder="UK, CA, US, ...">
+                    </label>
                 </div>
-
-                <button type="submit" class="btn btn-default">generate</button>
+                <div class=form-group">
+                    <label>State/Province
+                    <input type="text" class="form-control" id="csr-state" placeholder="...">
+                    </label>
+                </div>
+                <div class=form-group">
+                    <label>City/Locality
+                    <input type="text" class="form-control" id="csr-city" placeholder="...">
+                    </label>
+                </div>
+                <div class=form-group">
+                    <label>Organization
+                    <input type="text" class="form-control" id="csr-org" placeholder="Company Name">
+                    </label>
+                </div>
+                <div class=form-group">
+                    <label>Department / Unit
+                    <input type="text" class="form-control" id="csr-unit" placeholder="[optional]">
+                    </label>
+                </div>
+                <div class=form-group">
+                    <label>Domain or Common Name
+                    <input type="text" class="form-control" id="csr-dom" placeholder="example.com">
+                    </label>
+                </div>
             </form>
         </div>
         <div class="col-sm-6">
             <div class="panel">
                 CSR output goes here
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <form>
+
+                <h3>3. Your SSL Cert</h3>
+                <button type="submit" class="btn btn-default">generate</button>
+            </form>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel">
+                Cert output goes here
             </div>
         </div>
     </div>
