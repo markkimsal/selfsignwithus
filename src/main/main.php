8d5630c7264e477fd9adc53e7bf3a051a2d8f001
Mark Kimsal <metrofindings@gmail.com>
2015-03-02 10:35:30 -0500

    Add user message and redirect.

--- src/main/main.php
+++ src/main/main.php
@@ -2,10 +2,10 @@
 
 class Main_Main 
 {
-    public function keygenAction($request, $response, $session)
-    {
-        $bits = $request->cleanString('keygen-bits');
-        $type = $request->cleanString('keygen-type');
+	public function keygenAction($request, $response, $session)
+	{
+		$bits = $request->cleanString('keygen-bits');
+		$type = $request->cleanString('keygen-type');
 
 		if ($type != 'rsa' && $type != 'dsa') {
 			$type = 'rsa';
@@ -25,9 +25,14 @@ class Main_Main
 		if ($this->tempSave( implode("\n", $output) , 'rootkey', $session->sessionId )) {
 			$response->message = 'saved';
 			$response->addTo('main',  'You can download your root key file <a href="'.m_appurl('main/keygen/dl').'">here</a>.  This file download will expire in 10 minutes.');
+			$response->addUserMessage('You can download your root key file <a href="'.m_appurl('main/keygen/dl').'">here</a>.  This file download will expire in 10 minutes.', 'info');
+			$response->redir = m_appurl();
+
 		} else {
 			$response->statusCode = 500;
 			$response->addTo('main',  'Sorry, an internal error failed to generate your root key.  Please, try again.');
+			$response->addUserMessage('Sorry, an internal error failed to generate your root key.  Please, try again.', 'danger');
+			$response->redir = m_appurl();
 		}
 	}
 
