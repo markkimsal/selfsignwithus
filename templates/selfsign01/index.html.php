89c8d35cf3a44167a469d2b85aebf2896425f552
Colin Campbell <colin@elusivelabs.com>
2015-03-03 10:53:41 -0500

    Updated description and keyword metadata on homepage.

--- templates/selfsign01/index.html.php
+++ templates/selfsign01/index.html.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Easily create and manage your self signed certificates">
+    <meta name="keywords" content="self sign, self-signed, certificate, ssl, keygen, generator, manage, easy">
     <meta name="author" content="">
 
     <title>Self Signed SSL Certificates | Self Sign With Us</title>
